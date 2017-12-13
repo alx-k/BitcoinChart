c2524bf9861a200879bbd4055167fc4e83369777
blackhawk92 <8818177+blackhawk92@users.noreply.github.com>
2017-12-13 15:14:04 +0100

    Beispielconfig
    Example config added

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    accessToken: 'AABB ... ==', // a base64 encoded id ends with ==
+    widgetID: 'com.lametric.lotsOfNumbers' // basicly a URL of the Widget (app) without https://developer.lametric.com/api/V1/dev/widget/update/
+};
